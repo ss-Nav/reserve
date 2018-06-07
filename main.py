10a63f2fcbbc8062390cbf97ea7f72909541ae5c
ss-Nav <shishan@navigators.org.sg>
2018-06-07 11:10:12 +0800

    trying out on redis

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+import redis
+r = redis.StrictRedis(host='Reserve.redis.cache.windows.net',
+        port=6380, db=0, password='CuXrxyeDtPr80qkwgCUFhOvHWBbyyb+IVcSrrTKlbBs=', ssl=True)
+r.set('foo', 'tsuyo')
+
+
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  # return 'Hello, World!' + str(r.get('foo'))
+  return 'Hello, World!' + str(r.ping())
 
 if __name__ == '__main__':
   app.run()
